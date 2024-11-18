d76d60ca5844efe69b0e4808a2983729de65bd9a
LilJulian <juliadar267@gmail.com>
2024-11-18 11:17:03 -0500

    primer update

---
+++ app.js
@@ -0,0 +1,60 @@
+var saludo = "Muchachos";
+let adso = "2894667";
+const DNI = "1097096653"
+
+console.log(saludo);
+console.log(adso);
+{
+    let adso = "victor";
+    console.log(adso);
+}
+console.log(adso);
+console.log(saludo);
+console.log(window.saludo);
+console.log(window.adso);
+console.log(window);
+console.clear();
+
+let nombre = "Juan";
+let apellido = "Gómez"
+console.log(nombre.length);
+console.log(nombre[2]);
+let nCompleto = nombre + " " + apellido
+console.log(nCompleto.toUpperCase());
+console.log(nCompleto.includes("Freddy"));
+console.log(nCompleto.includes(""));
+console.clear();
+let item1 = "Item 1";
+let item2 = "Item 2";
+let item3 = "Item 3";
+let item4 = "Item 4";
+let item5 = "Item 5";
+
+let lista = '<ul>
+<li> ${item1}</li>
+<li> ${item2}</li>
+<li> ${item3}</li>
+<li> ${item4}</li>
+<li> ${item5}</li>
+</ul>';
+console.log(list);
+console.crear();
+
+// let entrada  = prompt("Ingrese el número");
+let uno = 5;
+let dos = new Number(2);
+console.log(dos);
+let flotante = 7.1542;
+console.log(flotante.toFixed(1));
+let nuevo = parseFloat(tres);
+if (typeof tres === "number") {
+    console.log(tres * nuevo);
+} else {
+    console.log("NO");
+}
+console.log(typeof entrada);
+console.log(typeof tres === "number")
+alert(entrada)
+
+confirm("Desean salir a comer?")
+//alert(respues)
---
+++ funciones.js
@@ -0,0 +1,41 @@
+// function nombre(){
+// console.log("esto es la función");
+// }
+// console.log(nombre());
+
+// function nombre(parametro = 250) {
+//     console.log(parametro);
+// }
+// nombre(4);
+
+
+// let respuesta = nombre(25);
+// function nombre(parametro) {
+// return parametro
+// }
+
+// let contador = 0;
+// for (let i = 1; i < numero; i++) {
+//     if (numero % i == 0) {
+//         contador++;
+//         }
+//     }
+//     if (contador > 2) {
+//         return false;
+//     } else {
+//         return true;
+//     }
+    
+// esPrimo(6);
+// console.log(respuesta);
+
+// const esPrimo = function (numero) {
+//     let contador = 0;
+// for (let i = 0; i < numero; i++) {
+//     if (numero % i == 0) {
+//         contador++;
+//         }
+//     }
+// }
+// esPrimo(6);
+// console.log(respuesta);
