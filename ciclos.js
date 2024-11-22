8d0ee967d167a35e31d627853fbf5c7409d4d75f
JuanDavidBG <davidbg581@gmail.com>
2024-11-22 11:22:57 -0500

    ultima update

--- ciclos.js
+++ ciclos.js
@@ -1,5 +1,46 @@
-let n = 8;
+// let n = 8;
+// let contador = 1;
+// while (contador <= n) {
+// console.log(contador);
+// contador++
+// }
 
-while (n <= 10) {
+// let n = 8;
+// let contador = 1;
+// do{
+//     console.log("?");
+// } while (false);
 
-}
+// for (let i = 0; ;i++){
+//     console.log
+// }
+
+// for (let i = 2; i <= 40; i += 2) {
+//     console.log(i);
+//   }
+
+function esPrimo(numero) {
+    if (numero <= 1) {
+      return false;
+    }
+    for (let i = 2; i <= Math.sqrt(numero); i++) {
+      if (numero % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  function encontrarPrimerosPrimos(n) {
+    let primos = [];
+    let numero = 2;
+    while (primos.length < n) {
+      if (esPrimo(numero)) {
+        primos.push(numero);
+      }
+      numero++;
+    }
+    return primos;
+  }
+  
+  console.log(encontrarPrimerosPrimos(5));
