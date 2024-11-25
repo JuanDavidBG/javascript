ba0d0f0e0b1044049dcfe24cefaf56475db4c877
JuanDavidBG <davidbg581@gmail.com>
2024-11-25 11:06:00 -0500

    ultimate

--- ciclos.js
+++ ciclos.js
@@ -19,28 +19,45 @@
 //     console.log(i);
 //   }
 
-function esPrimo(numero) {
-    if (numero <= 1) {
-      return false;
-    }
-    for (let i = 2; i <= Math.sqrt(numero); i++) {
-      if (numero % i === 0) {
-        return false;
-      }
-    }
-    return true;
-  }
+// function esPrimo(numero) {
+//     if (numero <= 1) {
+//       return false;
+//     }
+//     for (let i = 2; i <= Math.sqrt(numero); i++) {
+//       if (numero % i === 0) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
   
-  function encontrarPrimerosPrimos(n) {
-    let primos = [];
-    let numero = 2;
-    while (primos.length < n) {
-      if (esPrimo(numero)) {
-        primos.push(numero);
-      }
-      numero++;
-    }
-    return primos;
-  }
+//   function encontrarPrimerosPrimos(n) {
+//     let primos = [];
+//     let numero = 2;
+//     while (primos.length < n) {
+//       if (esPrimo(numero)) {
+//         primos.push(numero);
+//       }
+//       numero++;
+//     }
+//     return primos;
+//   }
   
-  console.log(encontrarPrimerosPrimos(5));
+//   console.log(encontrarPrimerosPrimos(5));
+
+// for (let i = 0; i < 10; i++) {
+//   if (i === 4) continue;
+//   console.log(` Item ${i}`); 
+// }
+
+const numeros = [1, 2, 3, 4];
+
+// let a = numeros[1]
+// let b = numeros[2]
+// let c = numeros[3]
+// let d = numeros[4]
+// console.log(a,b,c,d);
+
+let  [a, b, c , d] = numeros;
+
+console.log(a, b, c, d, e = 25);
